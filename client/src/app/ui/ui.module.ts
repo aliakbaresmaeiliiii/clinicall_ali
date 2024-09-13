d76cb726c1329c81922d3cd27a128427937ebbd7
aliakbaresmaeiliiii <aliakbaresmaeilisoft@gmail.com>
2024-09-13 16:11:15 +0800

    create footer

--- client/src/app/ui/ui.module.ts
+++ client/src/app/ui/ui.module.ts
@@ -1,20 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
-import { RouterModule, RouterOutlet, Routes } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { MatMenuModule } from '@angular/material/menu';
-import { ButtonComponent } from '../shared/components/button/button.component';
-import { SliderComponent } from './slider/slider.component';
-import { ScrollAnimationDirective } from '../shared/directives/scroll-animation/scoll-animation.directive';
-import { SocialMediaIconComponent } from '../shared/components/social-media-icon/social-media-icon.component';
-import { FeatureSectionComponent } from './feature-section/feature-section.component';
-import { CustomCardComponent } from '../shared/components/custom-card/custom-card.component';
 import { MatListModule } from '@angular/material/list';
-import { ServiceSectionComponent } from './service-section/service-section.component';
-import Swiper from 'swiper/bundle';
+import { MatMenuModule } from '@angular/material/menu';
+import { RouterModule, RouterOutlet, Routes } from '@angular/router';
+import { ButtonComponent } from '../shared/components/button/button.component';
+import { CustomCardComponent } from '../shared/components/custom-card/custom-card.component';
+import { SocialMediaIconComponent } from '../shared/components/social-media-icon/social-media-icon.component';
+import { ScrollAnimationDirective } from '../shared/directives/scroll-animation/scoll-animation.directive';
 import { BestDoctorsComponent } from './best-doctors/best-doctors.component';
 import { BlogComponent } from './blog/blog.component';
+import { FeatureSectionComponent } from './feature-section/feature-section.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ServiceSectionComponent } from './service-section/service-section.component';
+import { SliderComponent } from './slider/slider.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
 
 const routes: Routes = [{ path: '', component: HomeComponent }];
 
@@ -26,7 +30,8 @@ const routes: Routes = [{ path: '', component: HomeComponent }];
     FeatureSectionComponent,
     ServiceSectionComponent,
     BestDoctorsComponent,
-    BlogComponent
+    BlogComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
@@ -38,6 +43,10 @@ const routes: Routes = [{ path: '', component: HomeComponent }];
     SocialMediaIconComponent,
     CustomCardComponent,
     MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule
     
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
