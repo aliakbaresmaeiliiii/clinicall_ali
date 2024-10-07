39a0e3f25d52df16029b89867b3e565167eb7f5a
aliakbaresmaeiliiii <aliakbaresmaeilisoft@gmail.com>
2024-10-07 08:06:37 +0800

    edit inviroment

--- server/src/constants/constants_allowed_orginal.ts
+++ server/src/constants/constants_allowed_orginal.ts
@@ -1,7 +1,8 @@
 export const allowedOrigins = [
     'http://localhost:4200', // Frontend Web
     'http://localhost:3000', // Frontend Web
-    "http://192.168.50.207:8000"
-  ]
+    // "http://192.168.50.207:8000"
+    "http://172.26.144.1:8000"
+   ]
   
   
