7ae5a5af3da139dfdb13a20017fc5202329498c8
aliakbaresmaeiliiii <aliakbaresmaeilisoft@gmail.com>
2025-01-23 11:14:16 +0800

    remove node-modules  from git

--- client/src/app/ui/shared-ui/components/swiper-slider/swiper-slider.component.ts
+++ client/src/app/ui/shared-ui/components/swiper-slider/swiper-slider.component.ts
@@ -1,4 +1,15 @@
-import { Component, ElementRef, input, output, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  inject,
+  input,
+  OnInit,
+  output,
+  ViewChild,
+} from '@angular/core';
 import KeenSlider, { KeenSliderInstance } from 'keen-slider';
 import { SwiperData } from '../../models/swiper-slider';
 
@@ -11,19 +22,26 @@ import { SwiperData } from '../../models/swiper-slider';
   ],
   standalone: false,
 })
-export class SwiperSliderComponent {
+export class SwiperSliderComponent implements AfterViewInit, OnInit {
   swiperData = input<any[]>([]);
   isShowBtn = input<boolean>(false);
   title = input<string>('');
   currentSlide: number = 1;
   dotHelper: Array<Number> = [];
   onClick = output();
+  cdr = inject(ChangeDetectorRef);
 
   @ViewChild('sliderRef') sliderRef!: ElementRef<HTMLElement>;
 
-  slider!: KeenSliderInstance;
+  slider: KeenSliderInstance | null = null;
+
+  ngOnInit(): void {}
 
   ngAfterViewInit() {
+    this.initializeKeenSlider();
+  }
+
+  initializeKeenSlider() {
     setTimeout(() => {
       this.slider = new KeenSlider(this.sliderRef.nativeElement, {
         initial: this.currentSlide,
@@ -54,11 +72,10 @@ export class SwiperSliderComponent {
           },
         },
       });
-
       this.dotHelper = [
         ...Array(this.slider.track.details.slides.length).keys(),
       ];
-    }, 100);
+    }, 500);
   }
 
   ngOnDestroy() {
