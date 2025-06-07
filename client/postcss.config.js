1571453b50d85d3e651023d6c6818c6a9a936624
aliakbaresmaeiliiii <aliakbaresmaeilisoft@gmail.com>
2025-06-07 19:40:07 +0330

    migrate tailwind css to 4

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,9 +1,6 @@
-// postcss.config.js
 module.exports = {
-  plugins: [
-    require('@tailwindcss/postcss')({
-      config: './tailwindcss.config.js',
-    }),
-    require('autoprefixer'),
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
